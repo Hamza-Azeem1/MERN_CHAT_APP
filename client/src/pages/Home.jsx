8eebafc0011881f6950812f6e739cefd4ed08ae9
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-07-14 20:52:48 +0500

    updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,44 @@
-import { Outlet } from "react-router-dom"
+import axios from "axios"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Outlet, useNavigate } from "react-router-dom"
+import { logout, setUser } from "../redux/userSlice"
+import Sidebar from "../components/Sidebar"
 
 const Home = () => {
+
+    const user = useSelector(state => state.user)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const fetchUserDetails = async () => {
+        try {
+            const URL = `${import.meta.env.VITE_BACKEND_URL}/api/user-details`
+            const response = await axios({
+                url: URL,
+                withCredentials: true
+            })
+
+            dispatch(setUser(response.data.data))
+
+            if (response.data.data.logout) {
+                dispatch(logout())
+                navigate("/email")
+            }
+        } catch (error) {
+            console.log("error", error)
+        }
+    }
+
+    useEffect(() => {
+        fetchUserDetails()
+    }, [])
+
     return (
-        <div>
-            Home
+        <div className="grid lg:grid-cols-[300px, 1fr] h-screen max-h-screen">
+            <section className="bg-white">
+                <Sidebar />
+            </section>
 
             {/* message component */}
             <section>
--- server/controller/checkPassword.js
+++ server/controller/checkPassword.js
@@ -1,20 +1,18 @@
 const UserModel = require("../models/UserModel")
-const bcryptjs = require("bcryptjs")
+const bcryptjs = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 async function checkPassword(request, response) {
     try {
         const { password, userId } = request.body
 
-
         const user = await UserModel.findById(userId)
 
-        const verifyPassword = bcryptjs.compare(password, user.password)
-
+        const verifyPassword = await bcryptjs.compare(password, user.password)
 
         if (!verifyPassword) {
             return response.status(400).json({
-                message: "Please check password!",
+                message: "Please check password",
                 error: true
             })
         }
@@ -23,16 +21,15 @@ async function checkPassword(request, response) {
             id: user._id,
             email: user.email
         }
-
         const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
 
-        const cookiesOptions = {
+        const cookieOptions = {
             http: true,
             secure: true
         }
 
-        return response.cookie('token', token, cookiesOptions).status(200).json({
-            message: "Login Successfully!",
+        return response.cookie('token', token, cookieOptions).status(200).json({
+            message: "Login successfully",
             token: token,
             success: true
         })
