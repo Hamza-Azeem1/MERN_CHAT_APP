765b4aa3ca04a67a5232a7910958a7a7d99ba153
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-07-15 19:08:59 +0500

    updated

--- client/src/components/SearchUser.jsx
+++ client/src/components/SearchUser.jsx
@@ -7,14 +7,13 @@ import axios from "axios";
 import PropTypes from 'prop-types';
 
 const SearchUser = ({ onClose }) => {
-
     const [searchUser, setSearchUser] = useState([])
     const [loading, setLoading] = useState(false)
     const [search, setSearch] = useState("")
 
+
     const handleSearchUser = async () => {
         const URL = `${import.meta.env.VITE_BACKEND_URL}/api/search-user`
-
         try {
             setLoading(true)
             const response = await axios.post(URL, {
@@ -23,7 +22,6 @@ const SearchUser = ({ onClose }) => {
             setLoading(false)
 
             setSearchUser(response.data.data)
-
 
         } catch (error) {
             toast.error(error?.response?.data?.message)
@@ -34,15 +32,14 @@ const SearchUser = ({ onClose }) => {
         handleSearchUser()
     }, [search])
 
-
     return (
         <div className='fixed top-0 bottom-0 left-0 right-0 bg-slate-700 bg-opacity-40 p-2 z-10'>
             <div className='w-full max-w-lg mx-auto mt-10'>
-                {/* input search user */}
+                {/**input search user */}
                 <div className='bg-white rounded h-14 overflow-hidden flex '>
                     <input
                         type='text'
-                        placeholder='Search user by name or email....'
+                        placeholder='Search user by name, email....'
                         className='w-full outline-none py-1 h-full px-4'
                         onChange={(e) => setSearch(e.target.value)}
                         value={search}
@@ -52,8 +49,9 @@ const SearchUser = ({ onClose }) => {
                     </div>
                 </div>
 
-                {/* display search user*/}
+                {/**display search user */}
                 <div className='bg-white mt-2 w-full p-4 rounded'>
+                    {/**no user found */}
                     {
                         searchUser.length === 0 && !loading && (
                             <p className='text-center text-slate-500'>no user found!</p>
@@ -68,9 +66,9 @@ const SearchUser = ({ onClose }) => {
 
                     {
                         searchUser.length !== 0 && !loading && (
-                            searchUser.map((user, index) => {
+                            searchUser.map((user) => {
                                 return (
-                                    <UserSearchCard key={user._id + index} user={user} onClose={onClose} />
+                                    <UserSearchCard key={user._id} user={user} onClose={onClose} />
                                 )
                             })
                         )
@@ -80,12 +78,11 @@ const SearchUser = ({ onClose }) => {
                 </div>
             </div>
 
-            <div className='absolute top-0 right-0 text-2xl p-2 lg:text-4xl hover:text-white'>
+            <div className='absolute top-0 right-0 text-2xl p-2 lg:text-4xl hover:text-white' onClick={onClose}>
                 <button>
                     <IoClose />
                 </button>
             </div>
-
         </div>
     )
 }
