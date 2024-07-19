49df73fea69e57b81fb47ae1511e0ea25e054771
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-07-19 17:57:27 +0500

    updated

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,20 +1,63 @@
-import { IoChatbubbleEllipses } from 'react-icons/io5'
-import { FaUserPlus } from 'react-icons/fa'
-import { BiLogOut } from 'react-icons/bi'
-import { NavLink } from 'react-router-dom'
-import Avatar from '../components/Avatar'
-import { useSelector } from 'react-redux'
-import { useState } from 'react'
-import EditUserDetails from './EditUserDetails'
+import { useEffect, useState } from 'react'
+import { IoChatbubbleEllipses } from "react-icons/io5";
+import { FaUserPlus } from "react-icons/fa";
+import { NavLink, useNavigate } from 'react-router-dom';
+import { BiLogOut } from "react-icons/bi";
+import Avatar from './Avatar'
+import { useDispatch, useSelector } from 'react-redux';
+import EditUserDetails from './EditUserDetails';
 import { FiArrowUpLeft } from "react-icons/fi";
-import SearchUser from './SearchUser'
+import SearchUser from './SearchUser';
+import { FaImage } from "react-icons/fa6";
+import { FaVideo } from "react-icons/fa6";
+import { logout } from '../redux/userSlice';
 
 const Sidebar = () => {
-
     const user = useSelector(state => state?.user)
     const [editUserOpen, setEditUserOpen] = useState(false)
     const [allUser, setAllUser] = useState([])
     const [openSearchUser, setOpenSearchUser] = useState(false)
+    const socketConnection = useSelector(state => state?.user?.socketConnection)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (socketConnection) {
+            socketConnection.emit('sidebar', user._id)
+
+            socketConnection.on('conversation', (data) => {
+                console.log('conversation', data)
+
+                const conversationUserData = data.map((conversationUser) => {
+                    if (conversationUser?.sender?._id === conversationUser?.receiver?._id) {
+                        return {
+                            ...conversationUser,
+                            userDetails: conversationUser?.sender
+                        }
+                    }
+                    else if (conversationUser?.receiver?._id !== user?._id) {
+                        return {
+                            ...conversationUser,
+                            userDetails: conversationUser.receiver
+                        }
+                    } else {
+                        return {
+                            ...conversationUser,
+                            userDetails: conversationUser.sender
+                        }
+                    }
+                })
+
+                setAllUser(conversationUserData)
+            })
+        }
+    }, [socketConnection, user])
+
+    const handleLogout = () => {
+        dispatch(logout())
+        navigate("/email")
+        localStorage.clear()
+    }
 
     return (
         <div className='w-full h-full grid grid-cols-[48px,1fr] bg-white'>
@@ -30,6 +73,7 @@ const Sidebar = () => {
                         <FaUserPlus size={20} />
                     </div>
                 </div>
+
                 <div className='flex flex-col items-center'>
                     <button className='mx-auto' title={user?.name} onClick={() => setEditUserOpen(true)}>
                         <Avatar
@@ -40,7 +84,7 @@ const Sidebar = () => {
                             userId={user?._id}
                         />
                     </button>
-                    <button title='logout' className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded'>
+                    <button title='logout' className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded' onClick={handleLogout}>
                         <span className='-ml-2'>
                             <BiLogOut size={20} />
                         </span>
@@ -67,11 +111,57 @@ const Sidebar = () => {
                             </div>
                         )
                     }
-                </div>
 
+                    {
+                        allUser.map((conv, index) => {
+                            return (
+                                <NavLink to={"/" + conv?.userDetails?._id} key={index} className='flex items-center gap-2 py-3 px-2 border border-transparent hover:border-primary rounded hover:bg-slate-100 cursor-pointer'>
+                                    <div>
+                                        <Avatar
+                                            imageUrl={conv?.userDetails?.profile_pic}
+                                            name={conv?.userDetails?.name}
+                                            width={40}
+                                            height={40}
+                                        />
+                                    </div>
+                                    <div>
+                                        <h3 className='text-ellipsis line-clamp-1 font-semibold text-base'>{conv?.userDetails?.name}</h3>
+                                        <div className='text-slate-500 text-xs flex items-center gap-1'>
+                                            <div className='flex items-center gap-1'>
+                                                {
+                                                    conv?.lastMsg?.imageUrl && (
+                                                        <div className='flex items-center gap-1'>
+                                                            <span><FaImage /></span>
+                                                            {!conv?.lastMsg?.text && <span>Image</span>}
+                                                        </div>
+                                                    )
+                                                }
+                                                {
+                                                    conv?.lastMsg?.videoUrl && (
+                                                        <div className='flex items-center gap-1'>
+                                                            <span><FaVideo /></span>
+                                                            {!conv?.lastMsg?.text && <span>Video</span>}
+                                                        </div>
+                                                    )
+                                                }
+                                            </div>
+                                            <p className='text-ellipsis line-clamp-1'>{conv?.lastMsg?.text}</p>
+                                        </div>
+                                    </div>
+                                    {
+                                        Boolean(conv?.unseenMsg) && (
+                                            <p className='text-xs w-6 h-6 flex justify-center items-center ml-auto p-1 bg-primary text-white font-semibold rounded-full'>{conv?.unseenMsg}</p>
+                                        )
+                                    }
+
+                                </NavLink>
+                            )
+                        })
+                    }
+                </div>
             </div>
 
-            {/* edit user */}
+            {/**edit user details*/}
             {
                 editUserOpen && (
                     <EditUserDetails onClose={() => setEditUserOpen(false)} user={user} />
@@ -89,4 +179,4 @@ const Sidebar = () => {
     )
 }
 
-export default Sidebar;
+export default Sidebar
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,16 +1,14 @@
-const express = require("express")
+const express = require('express')
 const { Server } = require('socket.io')
 const http = require('http')
 const getUserDetailsFromToken = require('../helpers/getUserDetailsFromToken')
-const UserModel = require("../models/UserModel")
+const UserModel = require('../models/UserModel')
 const { ConversationModel, MessageModel } = require('../models/ConversationModel')
-//const getConversation = require('../helpers/getConversation')
+const getConversation = require('../helpers/getConversation')
 
 const app = express()
 
-
-// socket connection
-
+/***socket connection */
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
@@ -26,17 +24,17 @@ const onlineUser = new Set()
 io.on('connection', async (socket) => {
     const token = socket.handshake.auth.token
 
-    //current user details
+    //current user details 
     const user = await getUserDetailsFromToken(token)
 
-    //create a room.
-    socket.join(user?._id)
+    //create a room
+    socket.join(user?._id.toString())
     onlineUser.add(user?._id?.toString())
 
     io.emit('onlineUser', Array.from(onlineUser))
 
     socket.on('message-page', async (userId) => {
-        const userDetails = await UserModel.findById(userId).select('-password')
+        const userDetails = await UserModel.findById(userId).select("-password")
 
         const payload = {
             _id: userDetails?._id,
@@ -46,7 +44,19 @@ io.on('connection', async (socket) => {
             online: onlineUser.has(userId)
         }
         socket.emit('message-user', payload)
+
+
+        //get previous message
+        const getConversationMessage = await ConversationModel.findOne({
+            "$or": [
+                { sender: user?._id, receiver: userId },
+                { sender: userId, receiver: user?._id }
+            ]
+        }).populate('messages').sort({ updatedAt: -1 })
+
+        socket.emit('message', getConversationMessage?.messages || [])
     })
+
 
     //new message
     socket.on('new message', async (data) => {
@@ -93,17 +103,49 @@ io.on('connection', async (socket) => {
         io.to(data?.receiver).emit('message', getConversationMessage?.messages || [])
 
         //send conversation
-        // const conversationSender = await getConversation(data?.sender)
-        // const conversationReceiver = await getConversation(data?.receiver)
+        const conversationSender = await getConversation(data?.sender)
+        const conversationReceiver = await getConversation(data?.receiver)
 
         io.to(data?.sender).emit('conversation', conversationSender)
         io.to(data?.receiver).emit('conversation', conversationReceiver)
     })
 
 
+    //sidebar
+    socket.on('sidebar', async (currentUserId) => {
+        const conversation = await getConversation(currentUserId)
+
+        socket.emit('conversation', conversation)
+
+    })
+
+    socket.on('seen', async (msgByUserId) => {
+
+        let conversation = await ConversationModel.findOne({
+            "$or": [
+                { sender: user?._id, receiver: msgByUserId },
+                { sender: msgByUserId, receiver: user?._id }
+            ]
+        })
+
+        const conversationMessageId = conversation?.messages || []
+
+        const updateMessages = await MessageModel.updateMany(
+            { _id: { "$in": conversationMessageId }, msgByUserId: msgByUserId },
+            { "$set": { seen: true } }
+        )
+
+        //send conversation
+        const conversationSender = await getConversation(user?._id?.toString())
+        const conversationReceiver = await getConversation(msgByUserId)
+
+        io.to(user?._id?.toString()).emit('conversation', conversationSender)
+        io.to(msgByUserId).emit('conversation', conversationReceiver)
+    })
+
     //disconnect
     socket.on('disconnect', () => {
-        onlineUser.delete(user?._id)
+        onlineUser.delete(user?._id?.toString())
     })
 })
 
