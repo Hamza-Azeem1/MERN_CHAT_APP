cf1fd5df37d7e185471418875c8066a140558eeb
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2024-07-25 15:42:53 +0500

    updated

--- server/index.js
+++ server/index.js
@@ -1,28 +1,26 @@
-const express = require("express")
-const cors = require("cors")
-require('dotenv').config()
-const connectDB = require("./config/connectDB")
-const router = require("./routes/index")
-const cookiesParser = require("cookie-parser")
-const { app, server } = require('./socket/index')
-
-// const app = express()
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const connectDB = require("./config/connectDB");
+const router = require("./routes/index");
+const cookieParser = require("cookie-parser");
+const { app, server } = require('./socket/index'); // Importing app and server from the socket file
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true
-}))
+}));
 
-app.use(express.json())
-app.use(cookiesParser())
+app.use(express.json());
+app.use(cookieParser());
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
-//api endpoints
-app.use('/api', router)
+// API endpoints
+app.use('/api', router);
 
 connectDB().then(() => {
     server.listen(PORT, () => {
-        console.log(`Server started on PORT: ${PORT}`)
-    })
-})
+        console.log(`Server started on PORT: ${PORT}`);
+    });
+});
